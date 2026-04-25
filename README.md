0f6c795988ee971e42cd6bd5410e55679639969c
jose visbal <josevisbal144@gmail.com>
2026-04-24 20:53:59 -0500

    configuracion JS

---
+++ README.md
@@ -0,0 +1,8 @@
+este es mi portafolio personal donde presento mis habilidades, proyectos y conocimientos en desarrollo web 
+el diseño se enfoca en ser limpio sensillo y responsive 
+
+*HTML
+*CSS
+*JavaScript basico
+*flexbox
+
---
+++ script.js
@@ -0,0 +1,14 @@
+// Mensaje de bienvenida
+alert("Bienvenido a mi portafolio 🚀");
+
+// boton para bambiar el texto del parrafo
+function cambiarTexto() {
+    document.getElementById("texto").innerText =
+        "Ahora estás viendo un texto modificado con JavaScript 😎";
+}
+//este es un mensaje de saludo
+
+function mostrarMensaje() {
+    alert("Hola gracias por visitar mi portafolio 🙌");
+}
+
